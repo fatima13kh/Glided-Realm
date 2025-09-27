060a438f5c449fe7229244910c546dbabcce47cb
fatima13kh <fatima495khamis@gmail.com>
2025-09-27 16:18:00 +0300

    index page and new event form page are added as well as for the event controller

---
+++ controllers/events.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+
+const Event = require('../models/event');
+
+router.get('/', async (req, res) => {
+  try {
+    const populatedEvents = await Event.find({}).populate('owner');
+
+    res.render('events/index.ejs', {
+      events: populatedEvents,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+router.get('/new', (req, res) => {
+    res.render('events/new.ejs');
+});
+
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -16,6 +16,8 @@ const passUserToView = require('./middleware/pass-user-to-view.js');
 // Controllers
 const authController = require('./controllers/auth.js');
 
+const eventController = require('./controllers/events.js');
+
 // Set the port from environment variable or default to 3000
 const PORT = process.env.PORT ? process.env.PORT : '3000';
 
@@ -49,6 +51,8 @@ app.get('/', (req, res) => {
 });
 
 app.use('/auth', authController);
+
+app.use('/events', eventController);
 
 // PROTECTED
 
